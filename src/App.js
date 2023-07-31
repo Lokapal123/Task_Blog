58e2050907597aa3e6506a20cfc894259a408989
Lokapal02923 <20l31ao5l3@gmail.com>
2023-07-31 22:31:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Footer } from "./components/footer/Footer"
+import { Header } from "./components/header/Header"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { Regsiter } from "./pages/login/Regsiter"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Regsiter} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+export default App
